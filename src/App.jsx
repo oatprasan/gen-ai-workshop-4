7e94edbaa99cbc89c8c5ee07877bc2a365745765
Prasan Harnjit <prasan.h@kbtg.tech>
2025-08-27 11:26:22 +0700

    chore: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+      <h1>Hello World</h1>
+    </main>
+  )
+}
