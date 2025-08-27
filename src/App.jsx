763403f3ae016f268fe8f214689a5621e8bd9291
Prasan Harnjit <prasan.h@kbtg.tech>
2025-08-27 12:01:33 +0700

    chore: configure Tailwind CSS and demo

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 
 export default function App() {
   return (
-    <main style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
-      <h1>Hello World</h1>
+    <main className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-extrabold">Hello World</h1>
     </main>
   )
 }
